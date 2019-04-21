201c7170a721379a51569462131f0f7d27b83cd6
枫叶 <44088396@qq.com>
2019-04-21 22:47:12 +0800

    spring boot框架

---
+++ server-springboot/src/main/java/com/midcu/midcu/dao/UserDO.java
@@ -0,0 +1,54 @@
+package com.midcu.midcu.dao;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 用户实体类
+ */
+@Entity
+@Table(name = "auth_user")
+public class UserDO {
+    @Id
+    private Long id;
+    @Column(length = 32)
+    private String name;
+    @Column(length = 32)
+    private String account;
+    @Column(length = 64)
+    private String pwd;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
